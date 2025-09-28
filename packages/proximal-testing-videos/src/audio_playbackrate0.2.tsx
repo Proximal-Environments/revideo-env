d8b2bf5e599a6867d9324cf4636fb466045d97d3
justusmattern27 <justus.mattern@rwth-aachen.de>
2025-09-28 16:08:58 -0700

    playback rate videos

---
+++ packages/proximal-testing-videos/src/audio_playbackrate0.2.tsx
@@ -0,0 +1,74 @@
+import {makeProject, Vector2, useScene} from '@revideo/core';
+
+import {Audio, Img, makeScene2D, Video} from '@revideo/2d';
+import {all, createRef, waitFor} from '@revideo/core';
+
+/**
+ * The Revideo scene
+ */
+const scene = makeScene2D('scene', function* (view) {
+  const logoRef = createRef<Img>();
+
+  yield view.add(
+    <>
+      <Video
+        src={'https://revideo-example-assets.s3.amazonaws.com/stars.mp4'}
+        size={['100%', '100%']}
+        play={true}
+      />
+      <Audio
+        src={'https://revideo-example-assets.s3.amazonaws.com/chill-beat.mp3'}
+        play={true}
+        time={17.0}
+        playbackRate={0.2}
+      />
+    </>,
+  );
+
+  yield* waitFor(0.5);
+
+  view.add(
+    <Img
+      width={'1%'}
+      ref={logoRef}
+      src={
+        'https://revideo-example-assets.s3.amazonaws.com/revideo-logo-white.png'
+      }
+    />,
+  );
+
+  yield* all(logoRef().scale(40, 1.5), logoRef().rotation(90, 1.5));
+});
+
+
+export const project = makeProject({
+  name: 'project',
+  scenes: [scene],
+  variables: {
+    fill: 'green',
+  },
+  settings: {
+    shared: {
+      background: '#FFFFFF',
+      range: [0, Infinity],
+      size: new Vector2(640, 480),
+    },
+    preview: {
+      fps: 30,
+      resolutionScale: 1,
+    },
+    rendering: {
+      exporter: {
+        name: '@revideo/core/ffmpeg',
+        options: {
+          format: 'mp4',
+        },
+      },
+      fps: 30,
+      resolutionScale: 1,
+      colorSpace: 'srgb',
+    },
+  },
+});
+
+export default project;
