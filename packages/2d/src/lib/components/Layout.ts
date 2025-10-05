7a00587e40e66cc04904bacf7f59da8f99411a9c
root <root@localhost>
2025-10-05 07:37:34 +0000

    Restore feature text-align-2d-text

--- packages/2d/src/lib/components/Layout.ts
+++ packages/2d/src/lib/components/Layout.ts
@@ -101,6 +101,7 @@ export interface LayoutProps extends NodeProps {
   letterSpacing?: SignalValue<number>;
   textWrap?: SignalValue<TextWrap>;
   textDirection?: SignalValue<CanvasDirection>;
+  textAlign?: SignalValue<CanvasTextAlign>;
 
   size?: SignalValue<PossibleVector2<Length>>;
   offsetX?: SignalValue<number>;
@@ -284,6 +285,9 @@ export class Layout extends Node {
   @initial('inherit')
   @signal()
   public declare readonly textDirection: SimpleSignal<CanvasDirection, this>;
+  @defaultStyle('text-align')
+  @signal()
+  public declare readonly textAlign: SimpleSignal<CanvasTextAlign, this>;
 
   protected getX(): number {
     if (this.isLayoutRoot()) {
@@ -1000,7 +1004,9 @@ export class Layout extends Node {
       ? ''
       : `${this.letterSpacing()}px`;
 
-    
+    this.element.style.textAlign = this.textAlign.isInitial()
+      ? ''
+      : this.textAlign();
 
     if (this.textWrap.isInitial()) {
       this.element.style.whiteSpace = '';
